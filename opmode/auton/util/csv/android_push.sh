8efec54711b8cd723e62d52578d257a385b9593c
Grant Gao <ggao50@stuy.edu>
2024-03-27 09:29:13 -0400

    center auto changes

---
+++ opmode/auton/util/csv/android_push.sh
@@ -0,0 +1,11 @@
+py read_csv.py csv/FrontWallConstantsBlue.csv ../front/wall/FrontWallConstantsBlue.java
+py read_csv.py csv/FrontWallConstantsRed.csv ../front/wall/FrontWallConstantsRed.java
+
+py read_csv.py csv/FrontTrussConstantsBlue.csv ../front/truss/FrontTrussConstantsBlue.java
+py read_csv.py csv/FrontTrussConstantsRed.csv ../front/truss/FrontTrussConstantsRed.java
+
+py read_csv.py csv/BackDropWallConstantsBlue.csv ../backdrop/wall/BackDropWallConstantsBlue.java
+py read_csv.py csv/BackDropWallConstantsRed.csv ../backdrop/wall/BackDropWallConstantsRed.java
+
+py read_csv.py csv/BackDropTrussConstantsBlue.csv ../backdrop/truss/BackDropTrussConstantsBlue.java
+py read_csv.py csv/BackDropTrussConstantsRed.csv ../backdrop/truss/BackDropTrussConstantsRed.java
